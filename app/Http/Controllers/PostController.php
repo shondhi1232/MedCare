7c38307401fadb842b1df109a3aa85624a13aa73
shondhi <52061402+shondhi1232@users.noreply.github.com>
2022-04-08 01:18:18 +0600

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function addPost()
+    {
+
+        return view('addpost');
+    }
+
+    public function createPost(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return back()->with('post_created','Post has been created successfully');
+    }
+
+    public function getPost()
+    {
+        $posts = post::orderBy('id','DESC')->get();
+        return view('posts',compact('posts'));
+    }
+
+    public function deletePost($id)
+    {
+        Post::where('id',$id)->delete();
+        return back()->with('post_deleted', 'post has been delete sucessfully!');
+    }
+
+    public function editPost($id)
+        {
+            $Post=Post::find($id);
+            return view('editpost',compact('post'));
+        }
+}
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/aboutus',function(){
+    return view('aboutus');
+});
+
+Route::get('/review',function(){
+    return view('review');
+});
+Route::get('/contact',function(){
+    return view('contact');
+});
+
+
+route::get('/add-post',[App\Http\Controllers\PostController::class, 'addPost'])->name('add-post');
+route::post('/create-post',[App\Http\Controllers\PostController::class, 'createPost'])->name('post-create');
+route::get('/posts',[App\Http\Controllers\PostController::class, 'getPost']);
+route::get('/delete-post/{id}',[App\Http\Controllers\PostController::class, 'deletePost']);
+route::get('/edit-post/{id}',[App\Http\Controllers\PostController::class, 'editPost']);
